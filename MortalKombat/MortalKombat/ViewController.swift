84f0b15599d32e08d9475303007b464fb61d85f8
Gilmar Amaro <gilmar.amaro13@gmail.com>
2023-03-13 16:34:44 -0300

    Projeto criado

---
+++ MortalKombat/MortalKombat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MortalKombat
+//
+//  Created by Gilmar Amaro on 13/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
