72bbd8240209f29707f93df77717ab6082c9e8a9
Gilmar Amaro <gilmar.amaro13@gmail.com>
2023-03-13 17:11:40 -0300

    ...

---
+++ MortalKombat/Pods/Target Support Files/Pods-MortalKombat/Pods-MortalKombat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MortalKombat : NSObject
+@end
+@implementation PodsDummy_Pods_MortalKombat
+@end
---
+++ MortalKombat/Pods/Target Support Files/Pods-MortalKombatTests/Pods-MortalKombatTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MortalKombatTests : NSObject
+@end
+@implementation PodsDummy_Pods_MortalKombatTests
+@end
